9fbb3434b48e623c772bc10ce01a6570d65ecdeb
bahati7 <bahatiphilmon8@gmail.com>
2023-12-03 11:08:55 +0200

    created a function to test if a number is prime

--- exercice1.js
+++ exercice1.js
@@ -11,3 +11,15 @@ function printAllPrimes(N){
     const timeEnd = new Date().getTime();
     console.log('Time taken in milliseconds is:' + (timeEnd-timeStart));
 }
+function primeTest(number)    {
+    if(number<=1){
+        return false;
+        
+    }
+    for (let x=2;x<number;x++){
+        if(number % x == 0){
+            return false;
+        }
+    }
+    return true;
+}
