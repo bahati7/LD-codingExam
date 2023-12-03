b8f2346fa0d0c5d8ede55ce4b91d36cf134692e5
bahati7 <bahatiphilmon8@gmail.com>
2023-12-03 14:30:12 +0200

    created weekly intersetion function

--- exercice2.js
+++ exercice2.js
@@ -42,4 +42,65 @@ function findIntersectionTime(person1Availability, person2Availability, timezone
       console.log('\n====================================================');
     }
   }
+
+  function findWeeklyIntersection(person1WeeklyAvailability, person2WeeklyAvailability, timezone1, timezone2) {
+    const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const weeklyAvailability = {};
+  
+    for (let i = 0; i < days.length; i++) {
+      const day = days[i];
+      const person1Availability = person1WeeklyAvailability[i];
+      const person2Availability = person2WeeklyAvailability[i];
+  
+      const intersectionInterval = findIntersectionTime(person1Availability, person2Availability, timezone1, timezone2);
+      if (intersectionInterval) {
+        if (!weeklyAvailability[day]) {
+          weeklyAvailability[day] = [];
+        }
+        weeklyAvailability[day].push(intersectionInterval);
+      }
+    }
+  
+    console.log('Common availability in UTC for the entire week:');
+    for (const day in weeklyAvailability) {
+      const intervals = weeklyAvailability[day];
+      console.log(`${day}: ${intervals.map(interval => interval.toLocaleString('en-US', { timeZone: 'UTC' })).join(', ')}`);
+    }
+  
+    console.log('\nTranslate that to common availability ranges in time zone 1 and time timezone 2\n');
+    for (const day in weeklyAvailability) {
+      const intervals = weeklyAvailability[day];
+      console.log(
+        `${day}: ${intervals.map(interval =>
+          `${interval.toLocaleString('en-US', { timeZone: timezone1 })} - ${interval.toLocaleString('en-US', { timeZone: timezone2 })}`
+        ).join(', ')}`
+      );
+    }
+  }
+  
+  // Example usage with different timezones
+  const person1WeeklyAvailability = [
+    ['6:30 am', '3:30 pm'],
+    ['8:00 am', '5:00 pm'],
+    ['9:00 am', '2:00 pm'],
+    ['10:00 am', '3:00 pm'],
+    ['11:00 am', '4:00 pm'],
+    ['12:00 pm', '5:00 pm'],
+    ['1:00 pm', '6:00 pm'],
+  ];
+  
+  const person2WeeklyAvailability = [
+    ['8:30 am', '4:30 am'],
+    ['9:30 am', '6:00 pm'],
+    ['10:30 am', '7:00 pm'],
+    ['11:30 am', '8:00 pm'],
+    ['1:30 pm', '9:00 pm'],
+    ['2:30 pm', '10:00 pm'],
+    ['3:30 pm', '11:00 pm'],
+  ];
+  
+  const timezone1 = 'America/Los_Angeles';
+  const timezone2 = 'Europe/London';
+  
+  findWeeklyIntersection(person1WeeklyAvailability, person2WeeklyAvailability, timezone1, timezone2);
   
