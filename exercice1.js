16c60549592912cfc38a20e8846ef0b1c8161cb5
bahati7 <bahatiphilmon8@gmail.com>
2023-12-03 11:16:03 +0200

    added test case

--- exercice1.js
+++ exercice1.js
@@ -11,6 +11,7 @@ function printAllPrimes(N){
     const timeEnd = new Date().getTime();
     console.log('Time taken in milliseconds is:' + (timeEnd-timeStart));
 }
+//this function tests if a number is prime
 function primeTest(number)    {
     if(number<=1){
         return false;
@@ -23,3 +24,7 @@ function primeTest(number)    {
     }
     return true;
 }
+
+printAllPrimes(100);// time taken for N=100
+printAllPrimes(500);// time taken for N=500
+printAllPrimes(1000);// time taken for N=1000
