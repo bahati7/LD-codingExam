8eb86c8b935979f781117521f73e69fd580a5530
bahati7 <bahatiphilmon8@gmail.com>
2023-12-07 14:33:00 +0200

    updated the code

--- exercice3.js
+++ exercice3.js
@@ -3,6 +3,7 @@ function sliceFinder(A, S) {
     let countNum = 0;
     let sumNum = 0;
     let leftNum = 0;
+    const validSlices = [];
   
     for (let rightNum = 0; rightNum < A.length; rightNum++) {
       sumNum += A[rightNum];
@@ -14,30 +15,47 @@ function sliceFinder(A, S) {
   
       if (sumNum === S) {
         countNum++;
+        validSlices.push(A.slice(leftNum, rightNum + 1)); // Add slice to results
       }
     }
   
-    return countNum;
+    return{
+      countNum,validSlices,
+
+    } ;
   }
   
   const sizes = [10, 100, 1000];
-  const S = 100;
-  
-  for (const size of sizes) {
-    const A = randomArray(size, 1, 100);
-  
-    const startTime = performance.now();
-    const count = sliceFinder(A, S);
-    const endTime = performance.now();
-  
-    console.log(`N = ${size}, Time taken: ${endTime - startTime} ms, Count: ${count}`);
-  }
-  
-//generate the random array
+const S = 100;
+
+for (const size of sizes) {
+  const A = randomArray(size, 1, 100);
+
+  const startTime = performance.now();
+  const { countNum, validSlices } = sliceFinder(A, S);
+  const endTime = performance.now();
+
+  console.log(`N = ${size}, Time taken: ${endTime - startTime} ms, Count: ${countNum}`);
+  console.log(`Valid slices:`);
+  console.log(validSlices);
+}
+
+//testing example in the exercice
+const tab=[ 2, 4, 3 , 2, 1, 1, 5, 7];
+const SS=7;
+const startTime = performance.now();
+const { countNum, validSlices } = sliceFinder(tab, SS);
+const endTime = performance.now();
+console.log(`N = ${tab.length}, Time taken: ${endTime - startTime} ms, Count: ${countNum}`);
+console.log(`Valid slices:`);
+console.log(validSlices);
+
+
+//random array
 function randomArray(size, min, max) {
-    const array = [];
-    for (let x = 0; x < size; x++) {
-      array.push(Math.floor(Math.random() * (max - min + 1)) + min);
-    }
-    return array;
+  const array = [];
+  for (let i = 0; i < size; i++) {
+    array.push(Math.floor(Math.random() * (max - min + 1)) + min);
   }
+  return array;
+}
